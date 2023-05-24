adfe833eab42cdcf00036f49e79461a5f95fa094
Татьяна Антонова <t4n4uk@gmail.com>
2023-05-25 00:44:40 +0300

    перегрузка методов для 2чной СС, а там еще была 10чная, но я забыла написать

---
+++ src/main/java/org/example/CaclulatorBinary.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class CaclulatorBinary extends CalculatorDecimal {
+
+
+    @Override
+    public boolean setA(String x) {
+        try {
+            a = 0;
+            a = Integer.parseInt(x, 2);
+            System.out.println("число");
+            print(a);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ошибка превращения строки в бинарное число");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean setB(String x) {
+        try {
+            b = 0;
+            b = Integer.parseInt(x, 2);
+            System.out.println("число");
+            print(b);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ошибка превращения строки в бинарное число");
+            return false;
+        }
+    }
+}
