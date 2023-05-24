35551a5feddd888c554273f87a1ccce4c07819cd
Татьяна Антонова <t4n4uk@gmail.com>
2023-05-25 00:21:37 +0300

    заготовка менюшки и абстрактный класс

---
+++ src/main/java/org/example/AbstractCalculator.java
@@ -0,0 +1,94 @@
+package org.example;
+
+public abstract class AbstractCalculator {
+    int a = 0;
+    int b = 0;
+
+    static void print(int t) {
+        System.out.println("2СС = " + Integer.toBinaryString(t));
+        System.out.println("8СС = " + Integer.toOctalString(t));
+        System.out.println("10СС = " + t);
+        System.out.println("16СС = " + Integer.toHexString(t));
+    }
+
+
+    /**
+     * Метод устанавливает значение первого операнда
+     * @param x - строка, которую нужно преобразовать в число
+     * @return - успешность преобразования
+     */
+    public boolean setA(String x) {
+        try {
+            a = Integer.parseInt(x);
+            System.out.println("число");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ошибка преобразования строки в десятичное число");
+            return false;
+        }
+    }
+
+    /**
+     * Метод устанавливает значение второго операнда
+     * @param x - строка, которую нужно преобразовать в число
+     * @return - успешность преобразования
+     */
+    public boolean setB(String x) {
+        try {
+            b = Integer.parseInt(x);
+            System.out.println("число");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ошибка преобразования строки в десятичное число");
+            return false;
+        }
+    }
+
+    /**
+     * Метод сложения
+     * @return - результат сложения
+     */
+    public int plus() {
+        int result = a + b;
+        print(result);
+        return result;
+    }
+
+    /**
+     * Метод вычитания
+     * @return - результат вычитания
+     */
+    public int minus() {
+        int result = a - b;
+        print(result);
+        return result;
+    }
+
+    /**
+     * Метод умножения
+     * @return - результат умножения
+     */
+    public int multiplication() {
+        int result = a * b;
+        print(result);
+        return result;
+    }
+
+    /**
+     * Метод деления
+     * @return - результат деления
+     */
+    public int divide() {
+        try {
+            if (b == 0) {
+                throw new Exception("Нельзя делить на ноль");
+            }
+            int result = a / b;
+            print(result);
+            return result;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,188 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int inputForMenu(Scanner scanner) {
+        try {
+            int menuInput;
+            while (true) {
+                String inputText = scanner.nextLine();
+                if (inputText.matches("^\\d{0,10}$")) {
+                    menuInput = Integer.parseInt(inputText);
+                    break;
+                } else {
+                    throw new Exception("Введено не число");
+                }
+
+            }
+            return menuInput;
+        } catch (Exception e) {
+            System.out.println("Введите номер пункта меню еще раз");
+            return 0;
+        }
+    }
+
+    // 2сс 8сс 10сс 16сс, + - / * ,
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        boolean firstMenu = true;
+        boolean secondMenu = false;
+        int menuInput;
+
+        while (firstMenu) {
+            System.out.println(
+                    """
+                            \nВыберете систему счстления для вычисления:
+                            1. Двоичная
+                            2. Восьмеричная
+                            3. Десятичная
+                            4. Шестнадцатиричная
+                            5. Выход
+
+                            """);
+            menuInput = inputForMenu(scan);
+            switch (menuInput) {
+                case 1:
+                    System.out.println("Выбрана Двоичная система счистления\n");
+                    firstMenu = false;
+                    secondMenu = true;
+                    break;
+                case 2:
+                    System.out.println("Выбрана Восьмиричная система счистления\n");
+                    firstMenu = false;
+                    secondMenu = true;
+                    break;
+                case 3:
+                    System.out.println("Выбрана Десятичная система счистления\n");
+                    firstMenu = false;
+                    secondMenu = true;
+                    break;
+                case 4:
+                    System.out.println("Выбрана Шестнадцатиричная система счистления\n");
+                    firstMenu = false;
+                    secondMenu = true;
+                    break;
+                case 5:
+                    System.out.println("Выход из программы...\n");
+                    firstMenu = false;
+                    secondMenu = false;
+                    break;
+                default:
+                    System.out.println("Пункт меню с таким номером отсутсвует");
+                    firstMenu = true;
+                    secondMenu = false;
+                    break;
+            }
+            while (secondMenu) {
+                boolean firstNumber = false;
+                boolean secondNumber = false;
+                System.out.println(
+                        """
+                                \nВыберете операцию:
+                                1. Сложение
+                                2. Вычитание
+                                3. Умножение
+                                4. Деление
+                                5. Выбор системы счистления
+                                6. Выход
+                                """);
+                menuInput = inputForMenu(scan);
+                switch (menuInput) {
+                    case 1:
+
+                        System.out.println("Сложение\n");
+                        System.out.println("Введите первое число\n");
+                        System.out.println("Введите второе число\n");
+                        System.out.println();
+                        if(firstNumber && secondNumber)
+                        {System.out.println("Ответ:");}
+                        else {
+                            if(!firstNumber){
+                                System.out.println("Первое число введино неверно");
+                            }
+                            if(!secondNumber){
+                                System.out.println("Второе число введино неверно");
+                            }
+                        }
+                        System.out.println("---");
+                        firstMenu = false;
+                        secondMenu = true;
+                        break;
+                    case 2:
+                        System.out.println("Вычитание\n");
+                        System.out.println("Введите первое число\n");
+                        System.out.println("Введите второе число\n");
+                        System.out.println();
+                        if(firstNumber && secondNumber)
+                        {System.out.println("Ответ:");}
+                        else {
+                            if(!firstNumber){
+                                System.out.println("Первое число введино неверно");
+                            }
+                            if(!secondNumber){
+                                System.out.println("Второе число введино неверно");
+                            }
+                        }
+                        System.out.println("---");
+                        firstMenu = false;
+                        secondMenu = true;
+                        break;
+                    case 3:
+                        System.out.println("Умножение\n");
+                        System.out.println("Введите первое число\n");
+                        System.out.println("Введите второе число\n");
+                        System.out.println();
+                        if(firstNumber && secondNumber)
+                        {System.out.println("Ответ:");}
+                        else {
+                            if(!firstNumber){
+                                System.out.println("Первое число введино неверно");
+                            }
+                            if(!secondNumber){
+                                System.out.println("Второе число введино неверно");
+                            }
+                        }
+                        System.out.println("---");
+                        firstMenu = false;
+                        secondMenu = true;
+                        break;
+                    case 4:
+                        System.out.println("Деление\n");
+                        System.out.println("Введите первое число\n");
+                        System.out.println("Введите второе число\n");
+                        System.out.println();
+                        if(firstNumber && secondNumber)
+                        {System.out.println("Ответ:");}
+                        else {
+                            if(!firstNumber){
+                                System.out.println("Первое число введино неверно");
+                            }
+                            if(!secondNumber){
+                                System.out.println("Второе число введино неверно");
+                            }
+                        }
+                        System.out.println("---");
+                        firstMenu = false;
+                        secondMenu = true;
+                        break;
+                    case 5:
+                        firstMenu = true;
+                        secondMenu = false;
+                        break;
+                    case 6:
+                        System.out.println("Выход из программы...\n");
+                        firstMenu = false;
+                        secondMenu = false;
+                        break;
+                    default:
+                        System.out.println("Пункт меню с таким номером отсутсвует");
+                        firstMenu = false;
+                        secondMenu = true;
+                        break;
+                }
+            }
+        }
+
     }
 }
